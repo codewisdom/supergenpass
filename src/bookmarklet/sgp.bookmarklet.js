47a66ea624bf8b60cea663cdee65484aa76347ef
Sam Sellars <philosopher.sam@gmail.com>
2015-01-08 22:21:59 -0800

    Preparing to rebase my branch on master which is synced with upstream supergenpass repo.

--- src/bookmarklet/sgp.bookmarklet.js
+++ src/bookmarklet/sgp.bookmarklet.js
@@ -1,9 +1,9 @@
-(function ($) {
+void function ($) {
 
   // Configuration
-  var version = 20140728;
-  var domain = 'http://nocache.codewise.us';
-  var mobile = 'http://nocache.codewise.us/sgp/';
+  var version = 20141223;
+  var domain = 'https://chriszarate.github.io';
+  var mobile = 'https://chriszarate.github.io/supergenpass/mobile/';
   var minFrameArea = 100000;
   var loadTimeout = 2; // seconds
 
@@ -230,4 +230,4 @@
 
   }
 
-})(window.jQuery);
+}(window.jQuery);
